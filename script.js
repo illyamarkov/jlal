29fa1a49e396da1c7ae162c7e9d6f8a9a3b82cd4
illyamarkov <59452277+illyamarkov@users.noreply.github.com>
2023-09-26 20:36:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,77 @@
 const textElement = document.getElementById('scrolling-text');
 let scrollingInterval;
+let isMouseOver = false;
+let isResetting = false; // Flag to track if text reset is in progress
 
 function startScrollingText() {
     let text = "JL.AL";
+    let propability = 3;
+    let lastGeneratedText = "JL.AL";
 
     function scrollText() {
-        const randomChar = ['J', 'L', 'A', '-', '.', '_'][Math.floor(Math.random() * 6)];
-        text = text.slice(1) + randomChar;
-        textElement.textContent = text;
+        let randomChar = '_';
+        rnum = Math.floor(Math.random() * propability);
+        if (rnum === 0) {
+            randomChar = '-';
+            rnum = Math.floor(Math.random() * propability);
+            if (rnum === 0) {
+                randomChar = '.';
+                rnum = Math.floor(Math.random() * propability);
+                if (rnum === 0) {
+                    randomChar = 'L';
+                    rnum = Math.floor(Math.random() * propability);
+                    if (rnum === 0) {
+                        randomChar = 'J';
+                    }
+                }
+            }
+        }
+        lastGeneratedText = lastGeneratedText.slice(1) + randomChar;
+        textElement.textContent = lastGeneratedText;
+    }
+
+    function resetText() {
+        isResetting = true; // Set the flag to indicate resetting
+        clearInterval(scrollingInterval);
+        const targetText = "JL.AL";
+        let i = 0;
+        const resetInterval = setInterval(() => {
+            if (i < targetText.length) {
+                lastGeneratedText = lastGeneratedText.slice(1) + targetText[i];
+                textElement.textContent = lastGeneratedText;
+                i++;
+            } else {
+                clearInterval(resetInterval);
+                isResetting = false; // Reset the flag when reset is complete
+            }
+        }, 100);
     }
 
     textElement.addEventListener('mouseover', () => {
-        clearInterval(scrollingInterval);
-        scrollingInterval = setInterval(scrollText, 100);
+        if (!isMouseOver) {
+            clearInterval(scrollingInterval);
+            if (!isResetting) { // Check if not currently resetting
+                scrollingInterval = setInterval(scrollText, 100);
+                isMouseOver = true;
+            }
+        }
     });
 
     textElement.addEventListener('mouseout', () => {
-        console.log("Hello");
-        clearInterval(scrollingInterval);
-        textElement.textContent = "JL.AL";
-        text = "JL.AL";
+        if (!isResetting) { // Check if not currently resetting
+            resetText();
+            isMouseOver = false;
+        }
     });
 
-    // For mobile devices, click event starts scrolling for 5 seconds
     textElement.addEventListener('click', () => {
-        clearInterval(scrollingInterval);
-        scrollingInterval = setInterval(scrollText, 100);
+        if (!isMouseOver) {
+            clearInterval(scrollingInterval);
+            if (!isResetting) { // Check if not currently resetting
+                scrollingInterval = setInterval(scrollText, 100);
+                isMouseOver = true;
+            }
+        }
     });
 }
 
